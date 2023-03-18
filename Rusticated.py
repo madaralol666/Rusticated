f5397fad735e45b8e7bd2928b4421434f799053a
wolk gost <ds409060@gmail.com>
2023-03-18 12:27:48 +0300

    End

--- Rusticated.py
+++ Rusticated.py
@@ -3,9 +3,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Edge, EdgeOptions
 from selenium.webdriver.common.by import By
 from Server_name import Server_names
-import playsound
+from playsound import playsound
 import time
 import os
+import threading
 
 
 pars_link_account = "https://rusticated.com/"
@@ -18,7 +19,16 @@ driver = Edge(options=options)
 driver.get(pars_link_account)
 wait = WebDriverWait(driver, 10)
 
-input_server_name = input()
+input_server_name = input("Input server name: ")
+
+def sound_Air():
+    playsound('Sounds/Airdrop.mp3')
+def sound_Cargo():
+    playsound('Sounds/Cargoship.mp3')
+def sound_Helli():
+    playsound('Sounds/Helli.mp3')
+def sound_Chinook():
+    playsound('Sounds/Chinook.mp3')
 
 if input_server_name in Server_names:
     while True:
@@ -33,15 +43,19 @@ if input_server_name in Server_names:
         print("Chinook    ",event[3].text.split('\n')[1])
 
         if event[0].text.split('\n')[1] == "0h 0m 0s ago":
-            playsound.playsound('Sounds/Airdrop.mp3')
+            thd_sound_air = threading.Thread(target=sound_Air)
+            thd_sound_air.start()
 
         if event[1].text.split('\n')[1] == "0h 0m 0s ago":
-            playsound.playsound('Sounds/Cargoship.mp3')
+            thd_sound_cargo = threading.Thread(target=sound_Cargo)
+            thd_sound_cargo.start()
 
         if event[2].text.split('\n')[1] == "0h 0m 0s ago":
-            playsound.playsound('Sounds/Helli.mp3')
-            
+            thd_sound_Helli = threading.Thread(target=sound_Helli)
+            thd_sound_Helli.start()
+
         if event[3].text.split('\n')[1] == "0h 0m 0s ago":
-            playsound.playsound('Sounds/Chinook.mp3')
+            thd_sound_Chinook = threading.Thread(target=sound_Chinook)
+            thd_sound_Helli.start()
 else:
     print('Error: not found server name')
